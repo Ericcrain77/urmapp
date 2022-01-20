576edf1b3a6fa5cc8ec7bcfd76d8bf22e29b665f
Eric Crain <ericcrain77@gmail.com>
2022-01-19 21:54:10 -0500

    adding functionality to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,24 @@
-
-import React, { Component } from 'react';
+import React from 'react';
+// import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import LoginSignUpPage from './pages/LoginPage';
+import Homepage from './pages/Homepage';
 import './App.css';
-import * as am5 from "@amcharts/amcharts5";
-import * as am5map from "@amcharts/amcharts5/map";
-import usaHigh from "@amcharts/amcharts5-geodata/json/usaHigh";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 
-class App extends Component {
-  componentDidMount() {
-
-    var root = am5.Root.new("chartdiv");
-    
-    var chart = root.container.children.push(
-        am5map.MapChart.new(root, {
-          projection: am5map.geoAlbersUsa()
-        })
-        );
-
-
-    let polygonSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-         geoJSON: usaHigh
-      })
-    );
-    this.root = root;
-  }
-
-  componentWillUnmount() {
-    if (this.root) {
-      this.root.dispose();
-    }
-  }
-
-  render() {
-    return (
-      <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
-    );
-  }
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          {/* <Navbar /> */}
+          <Switch>
+            <Route exact path='/' component={LoginSignUpPage} />
+            <Route exact path='/saved' component={Homepage} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
