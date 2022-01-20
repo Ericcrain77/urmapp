b7b428a6c650a596bfecd0d2e24f2ea80314bcdc
Clfreeman22 <clfreeman22@gmail.com>
2022-01-19 20:02:00 -0600

    anamaited map

--- urmapp-react-app/src/App.js
+++ urmapp-react-app/src/App.js
@@ -9,20 +9,38 @@ import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 class App extends Component {
   componentDidMount() {
 
-    var root = am5.Root.new("chartdiv");
+    var root = am5.Root.new("mapdiv");
     
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
     var chart = root.container.children.push(
         am5map.MapChart.new(root, {
           projection: am5map.geoAlbersUsa()
         })
         );
-
-
+    
+      
     let polygonSeries = chart.series.push(
       am5map.MapPolygonSeries.new(root, {
-         geoJSON: usaHigh
+         geoJSON: usaHigh   
       })
     );
+    polygonSeries.mapPolygons.template.setAll({
+      tooltipText: "{name}",
+      interactive: true
+    });
+    
+    polygonSeries.mapPolygons.template.states.create("hover", {
+      fill: am5.color(0x677935)
+    });
+    
+    polygonSeries.data.push({
+      geometry: am5map.geoAlbersUsa(),
+      
+    });
+    
     this.root = root;
   }
 
@@ -34,7 +52,7 @@ class App extends Component {
 
   render() {
     return (
-      <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+      <div id="mapdiv" style={{ width: "100%", height: "500px" }}></div>
     );
   }
 }
