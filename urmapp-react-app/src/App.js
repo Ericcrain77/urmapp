eccdee8f94281d65a91362fc55f22e60de045e45
Clfreeman22 <clfreeman22@gmail.com>
2022-01-19 19:08:28 -0600

    map renders

--- urmapp-react-app/src/App.js
+++ urmapp-react-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import usaHigh from "@amcharts/amcharts5-geodata/json/usaHigh";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+
+class App extends Component {
+  componentDidMount() {
+
+    var root = am5.Root.new("chartdiv");
+    
+    var chart = root.container.children.push(
+        am5map.MapChart.new(root, {
+          projection: am5map.geoAlbersUsa()
+        })
+        );
+
+
+    let polygonSeries = chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+         geoJSON: usaHigh
+      })
+    );
+    this.root = root;
+  }
+
+  componentWillUnmount() {
+    if (this.root) {
+      this.root.dispose();
+    }
+  }
+
+  render() {
+    return (
+      <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+    );
+  }
 }
 
 export default App;
