56005aa3b9906ebfa0815e115ab544b7dd6a26ff
Brett herrell <brett_herrell@yahoo.com>
2022-01-23 19:38:37 -0600

    made states unique in the models

--- server/models/State.js
+++ server/models/State.js
@@ -1,24 +1,15 @@
 const { Schema, model } = require("mongoose");
 
-const stateSchema = new Schema(
-  {
-    state: {
-      type: String,
-      required: true,
-    },
-    visitedAt: {
-      type: Date,
-      default: Date.now,
-    },
-  }
-  // {
-  //   toJson: {
-  //     getters: true,
-  //   },
-  // }
-);
+const stateSchema = new Schema({
+  state: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  visitedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-// const State = model("State", stateSchema);
-
-// module.exports = State;
 module.exports = stateSchema;
