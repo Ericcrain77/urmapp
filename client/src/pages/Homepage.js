40459120a456c689b0dab525162afdbdc9cca660
Eric Crain <ericcrain77@gmail.com>
2022-01-23 22:57:53 -0500

    adding stylings to login/signup page and header of homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import '../App.css';
+import Map from '../components/Map';
 
 function HomePage() {
+
+
   return (
     <section>
       <header class='header'>
         <a href="/">
             <img class='header-logo' src="../public/logo164x101.png" alt="UrMapp Logo" />
         </a>
-        <h1>{Username}</h1>
+        <div class="header-right">
+            <h1>{Username}</h1>
+            <button type="submit" id='logout-btn'>Log Out</button>
+        </div>
       </header>
       <section class='page-body-2'>
         <Map />
       </section>
       <footer class='footer'>
         <h3>UrMapp © 2018</h3>
-        <p><a href="mailto:ericcrain77@gmail.com">Contact Us</a></p>
+        <p><a href="mailto:test@urmappinc.com">Contact Us</a></p>
       </footer>
     </section>
   )
-}
+};
 
 export default HomePage;
