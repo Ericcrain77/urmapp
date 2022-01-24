1783a6cb981446dda2e7180a9d76eee4e172f71f
Eric Crain <ericcrain77@gmail.com>
2022-01-23 21:24:42 -0500

    refactoring and adding homepage to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import React, { useState } from 'react';
 import LoginSignUpPage from './pages/LoginPage';
-import Homepage from './pages/Homepage';
+import HomePage from './pages/Homepage';
 import './App.css';
 
+
 function App() {
+  const [currentPage] = useState(" ");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "LoginSignUpPage":
+        return <LoginSignUpPage />;
+      case "HomePage":
+        return <HomePage />;
+      default:
+        return <LoginSignUpPage />;
+    }
+  };
+
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <>
-          {/* <Navbar /> */}
-          <Switch>
-            <Route exact path='/' component={LoginSignUpPage} />
-            <Route exact path='/saved' component={Homepage} />
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
-    </ApolloProvider>
+    <main>
+        {renderPage(currentPage)}
+    </main>
   );
 }
 
