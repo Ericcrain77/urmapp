65d1df6edb94ed872f034f3ba44accc80cbdb886
Eric Crain <ericcrain77@gmail.com>
2022-01-23 21:18:29 -0500

    adding stylings to the homepage

---
+++ client/src/components/Map.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import '../../src/App.css';
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import usaHigh from "@amcharts/amcharts5-geodata/json/usaHigh";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+
+var selectedStates = [
+  "US-CA",
+  "US-TX",
+  "US-TN",
+  "US-AL",
+  "US-AK",
+]
+
+  class Map extends Component {
+  
+  componentDidMount() {
+
+    var root = am5.Root.new("mapdiv");
+    
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
+    var chart = root.container.children.push(
+        am5map.MapChart.new(root, {
+          projection: am5map.geoAlbersUsa()
+        })
+        );
+    
+      
+    let polygonSeries = chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+         geoJSON: usaHigh,
+         fill: am5.color(0x5bc8af),
+         stroke: am5.color(0x000000)
+      })
+    )
+
+    polygonSeries.mapPolygons.template.setAll({
+      tooltipText: "{name}",
+      interactive: true,
+    });
+    polygonSeries.mapPolygons.template.states.create("hover", {
+      fill: am5.color(0xff6b6b)
+    });
+    
+    for (let i = 0; i < selectedStates.length; i++) {
+    polygonSeries.mapPolygons.template.adapters.add
+    ('fill', function(fill, target) {
+      if (target.dataItem.get("id") === selectedStates[i]) {
+        return am5.color(0xff6b6b)
+      }
+      else{
+        return fill;
+      }
+    })
+    };
+
+    this.root = root;
+  }
+
+  componentWillUnmount() {
+    if (this.root) {
+      this.root.dispose();
+    }
+  }
+
+  render() {
+    return (
+      <div id="mapdiv" style={{ width: "100%", height: "500px" }}></div>
+    );
+  }
+}
+
+export default Map;
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,77 +1,24 @@
-import React, { Component } from 'react';
-import '../../src/App.css';
-import * as am5 from "@amcharts/amcharts5";
-import * as am5map from "@amcharts/amcharts5/map";
-import usaHigh from "@amcharts/amcharts5-geodata/json/usaHigh";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import React from 'react';
+import '../App.css';
 
-var selectedStates = [
-  "US-CA",
-  "US-TX",
-  "US-TN",
-  "US-AL",
-  "US-AK",
-]
-
-  class Homepage extends Component {
-  
-  componentDidMount() {
-
-    var root = am5.Root.new("mapdiv");
-    
-    root.setThemes([
-      am5themes_Animated.new(root)
-    ]);
-
-    var chart = root.container.children.push(
-        am5map.MapChart.new(root, {
-          projection: am5map.geoAlbersUsa()
-        })
-        );
-    
-      
-    let polygonSeries = chart.series.push(
-      am5map.MapPolygonSeries.new(root, {
-         geoJSON: usaHigh,
-         fill: am5.color(0x5bc8af),
-         stroke: am5.color(0x000000)
-      })
-    )
-
-    polygonSeries.mapPolygons.template.setAll({
-      tooltipText: "{name}",
-      interactive: true,
-    });
-    polygonSeries.mapPolygons.template.states.create("hover", {
-      fill: am5.color(0xff6b6b)
-    });
-    
-    for (let i = 0; i < selectedStates.length; i++) {
-    polygonSeries.mapPolygons.template.adapters.add
-    ('fill', function(fill, target) {
-      if (target.dataItem.get("id") === selectedStates[i]) {
-        return am5.color(0xff6b6b)
-      }
-      else{
-        return fill;
-      }
-    })
-    };
-
-    this.root = root;
-  }
-
-  componentWillUnmount() {
-    if (this.root) {
-      this.root.dispose();
-    }
-  }
-
-  render() {
-    return (
-      <div id="mapdiv" style={{ width: "100%", height: "500px" }}></div>
-    );
-  }
+function HomePage() {
+  return (
+    <section>
+      <header class='header'>
+        <a href="/">
+            <img class='header-logo' src="../public/logo164x101.png" alt="UrMapp Logo" />
+        </a>
+        <h1>{Username}</h1>
+      </header>
+      <section class='page-body-2'>
+        <Map />
+      </section>
+      <footer class='footer'>
+        <h3>UrMapp © 2018</h3>
+        <p><a href="mailto:ericcrain77@gmail.com">Contact Us</a></p>
+      </footer>
+    </section>
+  )
 }
 
-export default Homepage;
+export default HomePage;
